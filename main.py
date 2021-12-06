509c758db7b374a2b35b627c4e065a3fa13d1645
Davi <davi.castelo.branco.almeida@gmail.com>
2021-12-06 17:50:57 +0000

    Added name stage

---
+++ main.py
@@ -0,0 +1,24 @@
+import time
+
+print("-" * 47)
+print("     Welcome to Dylan 2's Dating Adventure!    ")
+print("         Please insert your name in            ")
+print("              the box below                    ")
+print("-" * 47)
+time.sleep(5)
+
+name = input("What is your name:")
+
+print("So your name is " + name)
+time.sleep(2)
+print("...")
+time.sleep(1)
+print("...")
+time.sleep(1)
+print("...")
+time.sleep(3)
+print("NO ONE CARES LMAO")
+time.sleep(2)
+print("jk hehheh")
+time.sleep(1)
+print("Well your name is Dylan 2 ok, This is scripted don't blame me")
