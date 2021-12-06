7c26b0b923cea8bff366a685c8416a1f1b975509
Davi <davi.castelo.branco.almeida@gmail.com>
2021-12-06 18:09:25 +0000

    started main screen

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ print("              the box below                    ")
 print("-" * 47)
 time.sleep(5)
 
-name = input("What is your name:")
+name = input("What is your name: ")
 
 print("So your name is " + name)
 time.sleep(2)
@@ -22,3 +22,15 @@ time.sleep(2)
 print("jk hehheh")
 time.sleep(1)
 print("Well your name is Dylan 2 ok, This is scripted don't blame me")
+time.sleep(4)
+print("")
+print("")
+print("")
+print("Get ready to go in the world")
+
+lv = 0
+
+if lv == 0:
+  print()
+else:
+  print("Welcome back boss")
